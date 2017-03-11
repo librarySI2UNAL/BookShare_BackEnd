cd7ff8df40f61362a6086d56d00502723c2d0c67
jorge52an <jorgesolavila@gmail.com>
2017-03-10 22:42:51 -0500

    Chore: add foreign key in Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
 	attr_accessor :name, :description, :cover, :status, :author, :genre, :editorial, :year_of_publication, :code_type, :code
+	belongs_to :collection
 end
