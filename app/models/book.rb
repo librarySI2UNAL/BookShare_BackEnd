8b399424a7f40de570c9816abe1f8081cb850e7a
jorge52an <jorgesolavila@gmail.com>
2017-03-10 22:39:06 -0500

    Feat: add Book model

---
+++ app/models/book.rb
@@ -0,0 +1,3 @@
+class Book < ApplicationRecord
+	attr_accessor :name, :description, :cover, :status, :author, :genre, :editorial, :year_of_publication, :code_type, :code
+end
---
+++ db/migrate/20170311033700_create_books.rb
@@ -0,0 +1,18 @@
+class CreateBooks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.text :description
+      t.string :cover
+      t.string :status
+      t.string :author
+      t.string :genre
+      t.string :editorial
+      t.integer :year_of_publication
+      t.string :code_type
+      t.string :code
+
+      t.timestamps
+    end
+  end
+end
