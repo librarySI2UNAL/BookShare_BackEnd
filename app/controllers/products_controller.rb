f2a04f83538c8276384e37dc47ae02576e6aad33
jorge52an <jorgesolavila@gmail.com>
2017-03-27 17:35:28 -0500

    Chore: get product by id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,13 @@
 class ProductsController < ApplicationController
 	def index
-		@products = Product.where( available: true )
-		render json: { products: @products }
+		products = Product.where( available: true )
+		render json: { products: products }
 	end
 
 	def show
-
+		id = params[:id]
+		product = Product.find_by( id: id )
+		render json: { product: product }
 	end
 
 	def create
