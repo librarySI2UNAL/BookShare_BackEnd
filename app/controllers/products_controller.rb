cc37e9e90cfaef4ea4d037c5a3a3f7a803c8cacf
jorge52an <jorgesolavila@gmail.com>
2017-03-27 21:43:38 -0500

    Chore: add returns statements

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
 	def index
 		products = Product.where( available: true )
 		render json: { products: products }
+		return
 	end
 
 	def show
@@ -9,8 +10,10 @@ class ProductsController < ApplicationController
 		product = Product.find_by( id: id )
 		if product == nil
 			render json: { error: "Product doesn't exist" }, status: 404
+			return
 		else
 			render json: { product: product }
+			return
 		end
 	end
 
