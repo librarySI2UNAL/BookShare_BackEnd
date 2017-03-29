5a73dc67e6bf86482c33b2a1ba3aa6a9f855c0de
David <debeltranc@unal.edu.co>
2017-03-29 23:27:43 +0000

    chore: add password field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311043143) do
+ActiveRecord::Schema.define(version: 20170329232350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20170311043143) do
     t.integer  "city_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "password"
     t.index ["city_id"], name: "index_users_on_city_id", using: :btree
     t.index ["photo_id"], name: "index_users_on_photo_id", using: :btree
   end
