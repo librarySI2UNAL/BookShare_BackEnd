122ea3786b92df7009bd04f829a7db9ceca2d848
jorge52an <jorgesolavila@gmail.com>
2017-04-05 20:13:30 -0500

    Feat: add Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329232350) do
+ActiveRecord::Schema.define(version: 20170406010142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,16 @@ ActiveRecord::Schema.define(version: 20170329232350) do
     t.string   "code"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "product_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
   create_table "interests", force: :cascade do |t|
