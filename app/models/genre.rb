bbeb061fdbf551b80f102a8a0a0fe054f107af26
jorge52an <jorgesolavila@gmail.com>
2017-04-11 13:23:56 -0500

    Chore: add many to many relationship

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ApplicationRecord
+	has_many :interests, through: :interest_genre
+
 	validates :name, presence: true
 end
--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,6 +1,6 @@
 class Interest < ApplicationRecord
 	has_many :users, through: :user_interest
+	has_many :genres, through: :interest_genre
 
 	validates :name, presence: true
-	validates :icon_name, presence: true
 end
