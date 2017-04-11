5cbf4c688432e64c7dc5502f08f5fe741d1ebc5c
jorge52an <jorgesolavila@gmail.com>
2017-04-11 14:03:18 -0500

    Chore: add method load interest by id

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -3,4 +3,8 @@ class Interest < ApplicationRecord
 	has_many :genres, through: :interest_genre
 
 	validates :name, presence: true
+
+	def self.load_interest_by_id( id )
+		self.find_by_id( id )
+	end
 end
