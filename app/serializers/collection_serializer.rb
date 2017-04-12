46111510602b0a056b940cbfa40014fbf3c7656b
jorge52an <jorgesolavila@gmail.com>
2017-04-12 12:56:48 -0500

    Chore: update relations in serializers

--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -1,5 +1,5 @@
 class CollectionSerializer < ActiveModel::Serializer
 	attributes :name, :cover, :status, :author, :editorial, :year_of_publication, :code_type, :code
 
-	has_one :genre, serializer: GenreSerializer
+	belongs_to :genre, serializer: GenreSerializer
 end
