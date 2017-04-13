b4c49e7cf6b862c8d2d4d958215c207de8934e2d
jorge52an <jorgesolavila@gmail.com>
2017-04-13 12:43:40 -0500

    Feat: add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 	scope "/api/v1" do
 		post "login", to: "authentication#authenticate"
+		post "logout", to: "authentication#destroy"
+		resources :genres, only: [:index]
+		resources :interests, only: [:index]
 		resources :users, only: [:show, :create, :update, :destroy] do
 			get "products", to: "products#collection"
 			resources :products, only: [:create, :update, :destroy]
