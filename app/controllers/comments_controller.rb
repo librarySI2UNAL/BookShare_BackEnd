d6dcf5135d071c944e6c563e910dadaab1aa998c
mballeng91 <migaballengal@unal.edu.co>
2017-04-16 10:55:38 -0500

    Feature : Comments controller and DAOS

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,45 @@
 class CommentsController < ApplicationController
 	def create
+		comment = CommentsDAO.create_comment(comment_params)
+
+		if comment == nil
+			message = Message.not_found("User")
+			render json: {error: message}, status: 400
+			return
+		end
+
+		message = Message.object_created( "Comment" )
+		response = { message: message}
+		response[:data] = ActiveModelSerializers::SerializableResource.new( comment ).as_json[:comment]
+		render json: response
+
 	end
 
 	def update
+		comment = CommentsDAO.update_comment(params[:id].to_i, comment_params)
+		if comment == nil
+			message = Message.not_found("User or comment")
+			render json: {error: message}, status: 400
+			return
+		end
+		message = Message.object_created( "Comment" )
+		response = { message: message}
+		response[:data] = ActiveModelSerializers::SerializableResource.new( comment ).as_json[:comment]
+		render json: response
 	end
 
 	def destroy
+		deleted = CommentsDAO.delete_comment( params[:id].to_i )
+		if !deleted
+			message = Message.not_found( "Comment id" )
+			render json: { error: message }, status: 404
+			return
+		end
 	end
+
+	private
+	def comment_params
+		params.require( :data ).permit( :comment, :user_id )
+	end
+
 end
