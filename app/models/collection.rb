2e0120dcbe971ac9f80f140e246990960518a72d
jorge52an <jorgesolavila@gmail.com>
2017-04-16 19:29:13 -0500

    Chore: update validates in models

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,14 +3,10 @@ class Collection < ApplicationRecord
 	has_one :genre
 
 	validates :name, presence: true
-	validates :cover, presence: true
-	validates :status, presence: true
 	validates :author, presence: true
 	validates :genre, presence: true
 	validates :editorial, presence: true
 	validates :year_of_publication, presence: true
-	validates :code_type, presence: true
-	validates :code, presence: true
 
 	def self.load_collection_by_id( id )
 		self.find_by_id( id )
