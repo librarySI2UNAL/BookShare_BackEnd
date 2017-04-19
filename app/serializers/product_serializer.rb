954ac970f161ae35799b2a54f4b7ac872dbb120c
jorge52an <jorgesolavila@gmail.com>
2017-04-19 13:32:51 -0500

    Chore: general fixes

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,7 +1,7 @@
 class ProductSerializer < ActiveModel::Serializer
 	attributes :id, :special, :available, :cover, :status, :value, :code, :code_type
 
-	belongs_to :product_item, polymorphic: true
+	belongs_to :product_item, polymorphic: true, include: true
 	has_many :photos, serializer: PhotoSerializer
 	has_many :comments, serializer: CommentSerializer
 end
