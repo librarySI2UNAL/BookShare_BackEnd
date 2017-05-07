7823aba87a921be20abb483499d570beace8f0cb
Miguel Angel <migaballengal@unal.edu.co>
2017-05-07 14:44:24 -0500

    Chore: routes.rb final

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,21 @@ Rails.application.routes.draw do
 		resources :genres, only: [:index]
 		resources :interests, only: [:index]
 		get "users/validate", to: "users#validate_email"
-		resources :users, only: [:show, :create, :update, :destroy] do
+		get "users/search", to: "users#search"
+		get "users/qsearch", to: "users#qsearch"
+		get "products/search", to: "products#search"
+		get "products/qsearch", to: "products#qsearch"
+
+		resources :users, only: [:index, :show, :create, :update, :destroy] do
 			get "products", to: "products#collection"
 			resources :products, only: [:create, :update, :destroy]
 			resources :photos, only: [:create, :update, :destroy]
 		end
+
 		resources :products, only: [:index, :show] do
 			resources :comments, only: [:create, :update, :destroy]
 			resources :photos, only: [:create, :update, :destroy]
 		end
+
 	end
 end
