3f726cd5232112f233d17b5b59c610644aecdab3
Miguel Angel <migaballengal@unal.edu.co>
2017-05-07 14:42:04 -0500

    Fix : routes.rb refactor

--- config/routes.rb
+++ config/routes.rb
@@ -10,22 +10,15 @@ Rails.application.routes.draw do
 		get "products/search", to: "products#search"
 		get "products/qsearch", to: "products#qsearch"
 
-
 		resources :users, only: [:index, :show, :create, :update, :destroy] do
-
 			get "products", to: "products#collection"
 			resources :products, only: [:create, :update, :destroy]
 			delete "photos", to: "photos#destroy_collection"
 			resources :photos, only: [:create, :update, :destroy]
-
 		end
 
 		resources :products, only: [:index, :show] do
-
-
-
-			resources :comments, only: [:create, :update, :destroy, :q_search]
-
+			resources :comments, only: [:create, :update, :destroy]
 			delete "photos", to: "photos#destroy_collection"
 			resources :photos, only: [:create, :update, :destroy]
 		end
