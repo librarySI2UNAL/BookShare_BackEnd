e883de5b73aebc06df1ac6735e7bc7933c7c73b7
jorge52an <jorgesolavila@gmail.com>
2017-05-15 11:11:51 -0500

    Chore: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 	scope "/api/v1" do
+		
 		post "login", to: "authentication#authenticate"
-		resources :genres, only: [:index]
-		resources :interests, only: [:index]
 		get "users/validate", to: "users#validate_email"
 		get "users/search", to: "users#search"
-		get "users/qsearch", to: "users#qsearch"
 		get "products/search", to: "products#search"
-		get "products/qsearch", to: "products#qsearch"
+
+		resources :genres, only: [:index]
+		resources :interests, only: [:index]
 
 		resources :users, only: [:index, :show, :create, :update, :destroy] do
 			get "products", to: "products#collection"
@@ -19,6 +19,5 @@ Rails.application.routes.draw do
 			resources :comments, only: [:create, :update, :destroy]
 			resources :photos, only: [:create, :update, :destroy]
 		end
-
 	end
 end
